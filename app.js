cf03a8331016a89782b6fa30adcc8d5ce01f667b
maxthestranger <thestrangermax@gmail.com>
2021-08-17 23:40:26 +0300

    express setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
