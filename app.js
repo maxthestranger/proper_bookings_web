d5b61ff89383d6eba797fd2d3271ed191c8571db
maxthestranger <thestrangermax@gmail.com>
2021-08-18 10:59:40 +0300

    handling assets and static files

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/package/:id', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/new', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/checkout', (req, res) => {
   res.send('Hello world');
 });
 
